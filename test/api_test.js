afe38c4e68c8e3a4d2761a5f6328dea915ef5938
Paul Laskowski <pelaskowski@gmail.com>
2014-11-02 20:00:42 +0000

    refactoring tests to see what works

--- test/api_test.js
+++ test/api_test.js
@@ -1,10 +1,11 @@
 var chai = require('chai');
+var expect = require('chai').expect;
 var chaihttp = require('chai-http');
 var server = require('../server.js');
 
 chai.use(chaihttp);
 
-var webapp = 'http://localhost:8080'
+var webapp = 'http://localhost:8080';
 
 describe('Simple JSON API', function() {
     
@@ -12,8 +13,10 @@ describe('Simple JSON API', function() {
     // Used chai docs to make req and deal with res - https://www.npmjs.org/package/chai-http
     // Also peeked at jacshfr's pull request to properly form the .get method into a callback (was using .end to no avail)
     chai.request(webapp)
-      .get('/localtime', function() {
+      .get('/localtime')
+      .end(function(err, res) {
         expect(res).to.have.status(200);
+        expect(res).to.be.a('string');
         var d = new Date();
         expect(res).to.be.eql(d.toLocaleFormat() + ' blah!!');
       });
@@ -24,11 +27,12 @@ describe('Simple JSON API', function() {
   it('should greet someone', function () {
 
     chai.request(webapp)
-      .get('/myname/test', function() {
+      .get('/myname/test', function(err, res) {
         expect(res).to.have.status(200);
         expect(res).to.have.param('name1', 'test');
         expect(res).to.be.json;
       });
       
   });
+  
 });
