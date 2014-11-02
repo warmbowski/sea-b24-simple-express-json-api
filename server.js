f1d20bd1d78498188a8bc354d3aa8962c33feee8
Paul Laskowski <pelaskowski@gmail.com>
2014-11-02 06:44:12 +0000

    localtime refactored, and json name from parameter added

--- server.js
+++ server.js
@@ -1,31 +1,28 @@
 'use strict';
 var express = require('express');
-//var bodyParser = require('body-parser');
 
 var app = express();
-//app.use(bodyParser());
 var port = process.env.PORT;
 
 app.get('/', function(req, res){
   var html = 'Hello World';
-  
+  res.status(200);
   res.send(html);
 });
 
+//app.get code mainly built with research on http://expressjs.com/4x/api.html
+app.get('/myname/:name', function(req, res) {    //
+  var message = { msg : "Hello " + req.param('name') + "!" };
+  console.log(message);
+  res.status(200);
+  res.json(message);  //http://stackoverflow.com/a/10667299
+});
+
 app.get('/localtime', function(req, res) {
-  var localtime = Date.now();
-  var ldate = localtime.getFullYear().toString() + '-' +
-    localtime.getMonth().toString() + '-' +
-    localtime.getDate().toString();
-  var ltime = localtime.getHours().toString() + ':' +
-    localtime.getMinutes().toString() + ':' +
-    localtime.getSeconds().toString();
-  
-    
-  //res.sendStatus(200);
-  res.send(ldate + " " + ltime);
+  var localtime = new Date();
+  res.status(200);
+  res.send(localtime.toLocaleString());
 });
 
 app.listen(port);
-
 console.log('Server running on port: ' + port);
