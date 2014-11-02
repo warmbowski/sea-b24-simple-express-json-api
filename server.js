0bb40a4a92f89bae444fda5ffe845d6896393998
Paul Laskowski <pelaskowski@gmail.com>
2014-11-02 03:40:34 +0000

    first commit with initial local time api

--- server.js
+++ server.js
@@ -1,2 +1,31 @@
+'use strict';
 var express = require('express');
-//your code here
+//var bodyParser = require('body-parser');
+
+var app = express();
+//app.use(bodyParser());
+var port = process.env.PORT;
+
+app.get('/', function(req, res){
+  var html = 'Hello World';
+  
+  res.send(html);
+});
+
+app.get('/localtime', function(req, res) {
+  var localtime = Date.now();
+  var ldate = localtime.getFullYear().toString() + '-' +
+    localtime.getMonth().toString() + '-' +
+    localtime.getDate().toString();
+  var ltime = localtime.getHours().toString() + ':' +
+    localtime.getMinutes().toString() + ':' +
+    localtime.getSeconds().toString();
+  
+    
+  //res.sendStatus(200);
+  res.send(ldate + " " + ltime);
+});
+
+app.listen(port);
+
+console.log('Server running on port: ' + port);
